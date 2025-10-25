74d9b568ce4513ab91f429632f7b735413f63d38
thalytalima211 <thalytalima211@gmail.com>
2025-10-25 13:44:51 -0300

    fix: fixing assets bug

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-import { application } from "./application"
+import { application } from "application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 eagerLoadControllersFrom("controllers", application)
